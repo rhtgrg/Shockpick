00e845c769d56ea6b681247d5a4b0f879d84e058
Rohit Garg <crypto.lemy+github@gmail.com>
2014-10-27 21:29:27 -0700

    Added a tumbler, more playing with physics

---
+++ Assets/Driver.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Driver : MonoBehaviour {
+	float stuck = -1f;
+
+	void Update () {
+		if(stuck != -1f && transform.position.y < stuck) {
+			transform.position = new Vector3(transform.position.x, stuck, transform.position.z);
+		}
+
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.01f, 1 << LayerMask.NameToLayer("Tumbler"));
+		if (hit) {
+			stuck = transform.position.y;
+		}
+	}
+}
