84de580c5929c1b7ccfeefb606452358f5099d48
Rohit Garg <crypto.lemy+github@gmail.com>
2014-10-27 22:48:29 -0700

    Adding tutorial text (need to work on timings)

---
+++ Assets/Tutorial.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour {
+	public Text tutorialText;
+
+	void Start () {
+		StartCoroutine("TutorialText");
+	}
+
+	IEnumerator TutorialText () {
+		yield return new WaitForSeconds(1f);
+		tutorialText.text = "THESE ARE THE PINS >>";
+		yield return new WaitForSeconds(1f);
+		tutorialText.text = "PUSH THE CURRENT PIN";
+		yield return new WaitForSeconds(1f);
+		tutorialText.text = "THE CURRENT PIN IS <color='red'>RED</color>";
+		yield return new WaitForSeconds(1f);
+		tutorialText.text = "";
+		yield return null;
+	}
+}
