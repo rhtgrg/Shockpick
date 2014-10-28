658095afbea4b7f787251dfb15c27a9dcda8abfb
Rohit Garg <crypto.lemy+github@gmail.com>
2014-10-28 07:50:29 -0700

    Hid mouse and changed tutorial text.

--- Assets/CamSize.cs
+++ Assets/CamSize.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CamSize : MonoBehaviour {
 	void Start () {
+		Screen.showCursor = false;
 		Camera.main.orthographicSize = (Screen.height/2)/Constants.UNITS_PER_PIXEL;
 	}
 }
