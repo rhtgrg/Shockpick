02a67be1559c78334b34bb53b1eea928239a4ae7
Rohit Garg <crypto.lemy+github@gmail.com>
2014-10-27 21:13:22 -0700

    Made rotation more sensible

--- Assets/LockPick.cs
+++ Assets/LockPick.cs
@@ -5,12 +5,10 @@ public class LockPick : MonoBehaviour {
 	void Update () {
 		Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		// Target point is the right edge of the screen, rotate by inverting Y position
-		Vector3 targetDirection = (new Vector3(Screen.width, Screen.height/2-mousePos.y) - (Vector3) mousePos);
-		targetDirection.Normalize();
 		// Move lockpick with the mouse
 		transform.position = mousePos;
-		// Rotate lockpick based on target direction
-		float rot_z = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg * Constants.UNITS_PER_PIXEL;
-		transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 180);
+		// Rotate lockpick based on Y position
+		float rot_z = Mathf.Atan2((Input.mousePosition.y - Screen.height/2)/(Screen.height/2), Vector2.right.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
 	}
 }
