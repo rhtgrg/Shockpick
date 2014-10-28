7746ae65d250545f949163602aa79d6067b7d2ac
Rohit Garg <crypto.lemy+github@gmail.com>
2014-10-27 20:55:21 -0700

    Added script for pins

---
+++ Assets/Pin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+	float maxY;
+	float posX;
+	
+	void Start () {
+		posX = transform.position.x;
+		maxY = transform.position.y;
+	}
+	
+	void Update () {
+		// Pin can never move in the X direction at all
+		// Pin has a limit on how far it can fall in the Y direction
+		float posY = (transform.position.y < maxY) ? maxY : transform.position.y;
+		transform.position = new Vector3(posX, posY, transform.position.z);
+	}
+}
